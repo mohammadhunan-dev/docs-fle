b667104138baf7f56ee2842c99fc5cf002f3ab75
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-08-08 14:43:03 -0400

    added sample structure for gradle app

---
+++ examples/javaexample/src/main/java/com/example/fle/FleApplication.java
@@ -0,0 +1,21 @@
+package main.java.com.example.fle;
+
+
+import com.mongodb.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.model.vault.DataKeyOptions;
+import com.mongodb.client.vault.ClientEncryption;
+import com.mongodb.client.vault.ClientEncryptions;
+import com.mongodb.internal.HexUtils;
+import org.bson.BsonDocument;
+import org.bson.Document;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class FleApplication {
+
+}
